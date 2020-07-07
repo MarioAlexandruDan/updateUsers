82a8f5caeee2e0ed5d5e928506a3f87f07dfe49a
gabrielsinca <gabrielsinca14@gmail.com>
2020-07-07 08:03:23 +0300

    added more tests

--- updateUsers/src/test/java/ro/internship/services/GenerateUsersTest.java
+++ updateUsers/src/test/java/ro/internship/services/GenerateUsersTest.java
@@ -10,21 +10,37 @@ import ro.intrenship.classes.User;
 
 public class GenerateUsersTest {
 
-	@Test
-	public void test() {
+    @Test
+    public void test() {
 
-		Utils test = new Utils();
-		
-		Map<Integer, User> users = test.generateUsers();
-		
-		if (users.isEmpty()) {
-			fail("Your Map doesn't contain any users");
-		}
-		
-		if (users.size() != 10) {
-			fail("You should have exactly 10 users");
-		}
-		
-	}
+        Utils test = new Utils();
+
+        Map<Integer, User> users = test.generateUsers();
+
+        if (users.isEmpty()) {
+            fail("Your Map doesn't contain any users");
+        }
+
+        for (int i = 0; i < users.size(); i++) {
+            if (Integer.parseInt(users.get(i).getId()) < 0) {
+                fail("The user " + i + " id can't be less than 0");
+            }
+            if (Integer.parseInt(users.get(i).getId()) > 10) {
+                fail("The user " + i + " id can't be more than 10");
+            }
+            if (Integer.parseInt(users.get(i).getId()) != i + 1) {
+                fail("The user " + i + " id is not matching correct id");
+            }
+            if(users.get(i).getFirstName().isEmpty()) {
+                fail("The user " + i + "dosen't have a first name");
+            }
+            if(users.get(i).getLastName().isEmpty()) {
+                fail("The user " + i + "dosen't have a last name");
+            }
+            if(users.get(i).getBirthday().toString().isEmpty()) {
+                fail("The user " + i + "dosen't have a birthday");
+            }
+        }
+    }
 
 }
