5d1efc3c79408dfbdf031a7ec9a9748718ca53fc
MarioAlexandruDan <marioalexandrudan@gmail.com>
2020-07-09 19:15:52 +0300

    commit

--- userManagement/src/main/java/ro/management/program/App.java
+++ userManagement/src/main/java/ro/management/program/App.java
@@ -1,5 +1,10 @@
 package ro.management.program;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import ro.management.services.Utils;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,10 @@ public class App
 {
     public static void main( String[] args )
     {
+    	final Logger logger = LogManager.getLogger(App.class);
+    	
+    	logger.debug("Started the debug text file", Utils.generateUsers(5));
+    	
         System.out.println("IMPLEMENTED USERMANAGEMENT JAR FILE");
     }
 }
